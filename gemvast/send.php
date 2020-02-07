789ae3cc52f41fb6511c43efcdd320bd9bd94ab3
Igor Protsenko <protsxe51@gmail.com>
2020-02-07 16:08:51 +0200

    add send.php

---
+++ gemvast/send.php
@@ -0,0 +1,17 @@
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("protsxe@gmail.con", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From:protsxe@ukr.net \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}?>
