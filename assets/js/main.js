149753c57c63374eb55f55275b57e4dc6eda1be9
Igor Protsenko <protsxe51@gmail.com>
2020-02-18 13:37:03 +0200

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -327,6 +327,7 @@ jQuery(document).ready(function($) {
 
 
 
+
     $('.elementskit-navbar, .mobile-nav, .cta').find('a[href*="#"]:not([href="#"])').on('click', function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
